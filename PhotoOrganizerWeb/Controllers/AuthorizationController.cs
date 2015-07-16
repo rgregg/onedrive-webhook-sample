77025e9c3ddfdf9ef556c359709982719b707161
Ryan Gregg <rgregg@users.noreply.github.com>
2015-07-16 10:57:57 -0700

    Rename the web project

---
+++ PhotoOrganizerWeb/Controllers/AuthorizationController.cs
@@ -0,0 +1,118 @@
+﻿using CameraRollOrganizer.Utility;
+using Microsoft.OneDrive.Sdk;
+using PhotoOrganizerShared;
+using PhotoOrganizerShared.Models;
+using PhotoOrganizerShared.Utility;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace CameraRollOrganizer.Controllers
+{
+    public class AuthorizationController : ApiController
+    {
+
+        [HttpGet, Route("redirect")]
+        public async Task<IHttpActionResult> AuthRedirect(string code)
+        {
+            // Redeem authorization code for account information
+
+            OAuthHelper helper = new OAuthHelper(WebAppConfig.Default.MsaTokenService,
+                WebAppConfig.Default.MsaClientId,
+                WebAppConfig.Default.MsaClientSecret,
+                WebAppConfig.Default.MsaRedirectionTarget);
+
+            var token = await helper.RedeemAuthorizationCodeAsync(code);
+            if (null == token)
+            {
+                return JsonResponseEx.Create(HttpStatusCode.InternalServerError, new { message = "Invalid response from token service.", code = "tokenServiceNullResponse" });
+            }
+            
+            Account account = new Account(token);
+            OneDriveClient client = new OneDriveClient(WebAppConfig.Default.OneDriveBaseUrl, account, new HttpProvider(new Serializer()));
+
+            var rootDrive = await client.Drive.Request().GetAsync();
+
+            account.Id = rootDrive.Id;
+            account.DisplayName = rootDrive.Owner.User.DisplayName;
+
+            var existingAccount = await AzureStorage.LookupAccountAsync(rootDrive.Id);
+            if (null == existingAccount)
+            {
+                await AzureStorage.InsertAccountAsync(account);
+                existingAccount = account;
+            }
+            else
+            {
+                existingAccount.SetTokenResponse(token);
+                await AzureStorage.UpdateAccountAsync(existingAccount);
+            }
+
+            var authCookie = CookieForAccount(existingAccount);
+
+            return RedirectResponse.Create("/default.aspx", authCookie);
+        }
+
+
+        [HttpGet, Route("signout")]
+        public HttpResponseMessage SignOut()
+        {
+            var message = new HttpResponseMessage(HttpStatusCode.Redirect);
+            message.Headers.Add("Location", "/default.aspx");
+            message.Headers.AddCookies(new CookieHeaderValue[] { CookieForAccount(null) });
+
+            return message;
+        }
+
+        public static CookieHeaderValue CookieForAccount(Account account)
+        {
+            var nv = new NameValueCollection();
+            nv["id"] = null != account ? account.Id.Encrypt(WebAppConfig.Default.CookiePassword) : "";
+            //nv["token"] = null != account ? account.AccessToken.Encrypt(Config.CookiePassword) : "";
+
+            var cookie = new CookieHeaderValue("session", nv);
+            //cookie.Secure = true;
+            cookie.HttpOnly = true;
+            cookie.Expires = null != account ? DateTimeOffset.Now.AddMinutes(120) : DateTimeOffset.Now;
+            cookie.Path = "/";
+
+            return cookie;
+        }
+
+        public static async Task<Account> AccountFromCookie(HttpCookieCollection cookies)
+        {
+            var sessionCookie = cookies["session"];
+            if (null == sessionCookie) 
+                return null;
+
+            return await AccountFromCookie(sessionCookie.Values);
+        }
+
+        public static async Task<Account> AccountFromCookie(NameValueCollection storedCookieValue)
+        {
+            string accountId = null;
+            string encryptedAccountId = HttpUtility.UrlDecode(storedCookieValue["id"]);
+            if (null != encryptedAccountId)
+            {
+                accountId = encryptedAccountId.Decrypt(WebAppConfig.Default.CookiePassword);
+            }
+
+            if (null != accountId)
+            {
+                var account = await AzureStorage.LookupAccountAsync(accountId);
+                return account;
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ PhotoOrganizerWeb/default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Async="true" Inherits="CameraRollOrganizer._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>OneDrive Camera Roll Organizer (sample)</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>OneDrive Camera Roll Organizer</h1>
+
+        <asp:Panel runat="server" ID="panelNoCurrentUser">
+            <p>Sign in to your OneDrive to enable automatic camera roll organization. This sample application
+            uses Webhooks and OneDrive API.</p>
+
+            <p><asp:HyperLink runat="server" ID="linkToSignIn">Sign in to OneDrive</asp:HyperLink></p>
+        </asp:Panel>
+
+        <asp:Panel runat="server" ID="panelLoggedIn" Visible="false">
+            <p>Account owner: <asp:Label runat="server" ID="labelAccountDisplayName"></asp:Label></p>
+            <p>ID: <asp:Label runat="server" ID="labelAccountId"></asp:Label></p>
+            <p>Files organized: <asp:Label runat="server" ID="labelPhotosOrganizedCount"></asp:Label></p>
+            <p>Webhooks processed: <asp:Label runat="server" ID="labelWebhooksReceived"></asp:Label></p>
+            <p>Folder Format (date format string): <asp:TextBox runat="server" ID="textBoxFolderFormatString"></asp:TextBox></p>
+            <p>Source Special Folder ID (approot or cameraroll): <asp:TextBox runat="server" ID="textBoxSourceFolder"></asp:TextBox></p>
+            <p><asp:CheckBox runat="server" ID="checkBoxEnableAccount" Text="Enable Moving Items (unchecked we'll just count webhooks received)" /></p>
+            <p><asp:Button runat="server" ID="buttonSaveChanges" Text="Save Changes" OnClick="buttonSaveChanges_Click" /></p>
+            
+            <asp:Label runat="server" ID="labelErrors" ForeColor="Red" Text=""></asp:Label>
+
+            <p><a href="/signout">Sign Out</a></p>
+            <p><a href="/api/action/createfile" target="_blank">Create Test File</a></p>
+            <p><a href="/api/action/subscriptions?path=jump/apps/webhook%20sample"  target="_blank">View subscriptions</a></p>
+        </asp:Panel>
+    </div>
+    </form>
+</body>
+</html>
